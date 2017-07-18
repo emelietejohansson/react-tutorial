db7ef6a6349c70c2747b9ecc6f0f552e5d6286f6
Emelie Johansson <emelietejohansson@gmail.com>
2017-07-18 11:40:25 +0200

    Intro to React components
    Created a simple component and rendered it

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,15 @@
-alert('It wooooorks!');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Create component
+var TodoComponent = React.createClass({
+    render: function(){
+        return(
+            <h1>Hello</h1>
+        );
+    }
+});
+
+
+// Put component into HTML page
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
